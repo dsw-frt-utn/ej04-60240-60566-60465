94e3e90b948ebde4797bb42fa720579dbfa531a9
campos ana sofia <sofiacampos1824@gmail.com>
2026-04-17 22:23:52 -0300

    Guardado temporal

---
+++ src/main/java/domain/Marca.java
@@ -0,0 +1,55 @@
+<<<<<<< HEAD
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+=======
+
+>>>>>>> feature/marcas
+package domain;
+
+/**
+ *
+ * @author sofia
+ */
+public class Marca {
+<<<<<<< HEAD
+    private String nombre,
+    private String pais;
+=======
+    private String nombre;
+   private String pais;
+>>>>>>> feature/marcas
+
+    public Marca(String nombre, String pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+<<<<<<< HEAD
+=======
+    public Marca(String nombre) {
+        this.nombre = nombre;
+    }
+
+>>>>>>> feature/marcas
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+<<<<<<< HEAD
+    
+=======
+>>>>>>> feature/marcas
+}
